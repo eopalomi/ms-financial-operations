d902fda685d9d719f1e6de4fa8b31e50e9432380
Erick <eopalomi@gmail.com>
2023-07-14 10:48:40 -0500

    update dockerfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'FinancialOperations',
-        script: 'dist/app.js',
+        script: 'build/app.js',
         instances: 1,
         autorestart: true,
         watch: false,
